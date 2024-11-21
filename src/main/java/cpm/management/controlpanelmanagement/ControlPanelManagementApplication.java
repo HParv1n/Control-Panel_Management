51a71d792a47827123a55e13f068aa2bc0f04521
hacagayevph <Parvin.Hacagayev@kapitalbank.az>
2024-11-21 20:33:52 +0400

    Initial commit for develop branch

---
+++ src/main/java/cpm/management/controlpanelmanagement/ControlPanelManagementApplication.java
@@ -0,0 +1,13 @@
+package cpm.management.controlpanelmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlPanelManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlPanelManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cpm/management/controlpanelmanagement/ControlPanelManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package cpm.management.controlpanelmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlPanelManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
